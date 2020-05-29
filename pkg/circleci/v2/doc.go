0951185d54731653e2bd6753eb071617f4b4f762
Koichi Shiraishi <zchee.io@gmail.com>
2020-05-29 17:29:13 +0900

    circleci/v2: add v2 package

---
+++ pkg/circleci/v2/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2020 The ccictl Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package v2 provide the CircleCI v2 API.
+package v2
